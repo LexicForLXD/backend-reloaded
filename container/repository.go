e8f242f140fe734d6d0d73ec7efced0a4e29612f
Leon Lenzen <ich@leonlenzen.de>
2019-02-13 14:16:17 +0100

    refactor: container repo

--- container/repository.go
+++ container/repository.go
@@ -11,6 +11,7 @@ type Repository interface {
 	FetchByHost(ctx context.Context, limit int, offset int, hostID string) ([]*models.Container, error)
 	GetByID(ctx context.Context, ID string) (*models.Container, error)
 	GetByName(ctx context.Context, name string) (*models.Container, error)
+	Get(ctx context.Context, field string, value string) (*models.Container, error)
 	Update(ctx context.Context, container *models.Container) error
 	Store(ctx context.Context, container *models.Container) error
 	Delete(ctx context.Context, id string) error
--- container/repository/container.go
+++ container/repository/container.go
@@ -1,1 +1,44 @@
 package repository
+
+import (
+	"context"
+
+	"github.com/jinzhu/gorm"
+	"github.com/lexicforlxd/backend-reloaded/container"
+	"github.com/lexicforlxd/backend-reloaded/models"
+)
+
+type containerRepository struct {
+	db *gorm.DB
+}
+
+func NewContainerRepository(db *gorm.DB) container.Repository {
+	return &containerRepository{db}
+}
+
+func (cr *containerRepository) Fetch(ctx context.Context, limit int, offset int) ([]*models.Container, error) {
+	var containers []*models.Container
+
+	if err := cr.db.Limit(limit).Offset(offset).Order("name").Find(&containers).Error; err != nil {
+		return nil, err
+	}
+
+	return containers, nil
+}
+
+// Set all the fields in the Container struct you want to search for
+func (cr *containerRepository) Get(ctx context.Context, searchedContainer models.Container) (*models.Container, error) {
+	container := models.Container{}
+	if err := cr.db.Or(searchedContainer).First(&container).Error; err != nil {
+		return nil, err
+	}
+	return &container, nil
+}
+
+func (cr *containerRepository) GetByID(ctx context.Context, ID string) (*models.Container, error) {
+	container := models.Container{}
+	if err := cr.db.Where("id = ?", ID).First(&container).Error; err != nil {
+		return nil, err
+	}
+	return &container, nil
+}
